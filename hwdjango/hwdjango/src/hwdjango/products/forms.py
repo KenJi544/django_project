77350eeeab61480f8bbcc2f2c4d2c5925de1d5aa
KenJi544 <dan.baltag.9878@gmail.com>
2018-07-21 11:16:08 +0300

    updated the code

---
+++ hwdjango/hwdjango/src/hwdjango/products/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder':'Type in the title !*'
+            }
+        ))
+    description = forms.CharField(required=False,
+            widget=forms.Textarea(
+                attrs={
+                    'placeholder':'Type in the description',
+                    'rows':20,
+                    'cols':150,
+                    }
+                ))
+    price = forms.DecimalField(initial=0.00)
+
+    class Meta:
+        model = Product
+        fields = {
+                'title',
+                'description',
+                'price'
+                }
+
+    def clean_price(self,*args,**kwargs):
+        price = self.cleaned_data.get('price')
+        if price < 0.0:
+            raise forms.ValidationError('Not a valid price')
+        return price
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'placeholder':'Type in the title !*'
+            }
+        ))
+    description = forms.CharField(required=False,
+            widget=forms.Textarea(
+                attrs={
+                    'placeholder':'Type in the description',
+                    'rows':20,
+                    'cols':150,
+                    }
+                ))
+    price = forms.DecimalField(initial=0.00)
+
