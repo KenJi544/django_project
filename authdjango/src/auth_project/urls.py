60aff7dc537a5c9a8e645dd9969f2b3287c4f70f
KenJi544 <dan.baltag.9878@gmail.com>
2018-08-10 17:09:57 +0300

    auth app is ready, need to work on hwdjango auth

--- authdjango/src/auth_project/urls.py
+++ authdjango/src/auth_project/urls.py
@@ -20,5 +20,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
